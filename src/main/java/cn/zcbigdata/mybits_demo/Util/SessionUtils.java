996ca9aaef75ff369be55099b150e3283a2ab8c5
刘泽 <645489459@qq.com>
2021-03-26 20:03:49 +0800

    SessionUtils.java更改

--- src/main/java/cn/zcbigdata/mybits_demo/Util/SessionUtils.java
+++ src/main/java/cn/zcbigdata/mybits_demo/Util/SessionUtils.java
@@ -7,7 +7,7 @@ public class SessionUtils {
      * 用于检查是否登录的方法
      *
      * @param session 一个HttpSession对象
-     * @param roleId    期望的用户类型：1-系统管理员；2-门店管理员；3-系统管理员+门店管理员；4-销售人员；5-系统管理员+销售人员；6-门店管理员+销售人员；7-所有登录用户
+     * @param roleId    期望的用户类型：1-系统管理员；2-老师；3-系统管理员+老师；4-学生；5-系统管理员+学生；6-老师+学生；7-所有登录用户
      * @return 用户已经登录返回true，否则返回false
      */
     public static boolean checkLogin(HttpSession session, int roleId) {
